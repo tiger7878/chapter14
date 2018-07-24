cb88cdefa0fe168f9bda02684b82f63040306397
tiger7878 <zhcw63@163.com>
2018-07-24 09:43:21 +0800

    增加hibernate注解方式配置ORM

---
+++ src/test/java/com/smart/dao/hibernateAnno/ForumHibernateDaoAnnoTest.java
@@ -0,0 +1,29 @@
+package com.smart.dao.hibernateAnno;
+
+import com.smart.dao.hibernate.ForumHibernateDao;
+import com.smart.domain.Forum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+/**
+ * User: monkey
+ * Date: 2018-07-24 9:40
+ */
+@ContextConfiguration("classpath:applicationContext-hbt-anno.xml")
+public class ForumHibernateDaoAnnoTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private ForumHibernateDao forumHibernateDao;
+
+    @Test
+    public void testaddForum(){
+        Forum forum=new Forum();
+        forum.setForumId(7);
+        forum.setForumName("c");
+        forum.setForumDesc("c1");
+        forumHibernateDao.addForum(forum);
+    }
+
+}
